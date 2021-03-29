b04e5d3ac98c5b5d85904d87c4fd182419abe6c2
Adrien <adrien@maranville.io>
2021-03-28 19:28:06 -0600

    Adds html prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -3,4 +3,18 @@ module.exports = {
   tabWidth: 2,
   semi: true,
   singleQuote: true,
+  overrides: [
+    {
+      files: '*.test.js',
+      options: {
+        semi: true,
+      },
+    },
+    {
+      files: ['*.html'],
+      options: {
+        tabWidth: 4,
+      },
+    },
+  ],
 };
