74e9f7e73b5b1bf109080e47ddde634a04032914
Adrien <adrien@maranville.io>
2021-03-28 18:47:27 -0600

    🐣 Creates default site

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  purge: [
+    './*.html',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+        colors: {
+            orange: colors.orange,
+            teal: colors.teal,
+            cyan: colors.cyan,
+            'light-blue': colors.lightBlue
+        }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
